0369ca690680a06e97873e81f7b694db627a55de
Humayd Khan <me@dyamuh.dev>
2026-02-23 17:41:11 -0600

    no dtfiles installer

--- setup/post-arch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export RUNZSH=no   # prevents auto-launch and overwriting .zshrc
-export CHSH=no     # prevents changing your default shell
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-
-echo "✅ Installation complete"
